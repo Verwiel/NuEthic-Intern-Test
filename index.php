8bf83e1fdd98090f79cc71360cf3565278b7bc3b
Drew <verwiel.code@gmail.com>
2019-08-08 15:10:43 -0600

    heroku test

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php 
   include_once("index.html");
 
+
+  if (isset($_POST['submit'])) {
   $name = $_POST['name'];
-  $email = $_POST['email'];
-  $phone = $_POST['phone'];
+  $subject = $_POST['phone'];
+  $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
-
-  $formcontent="From: $name \n 
-                Phone Number: $phone \n 
-                Message: $message";
-
-  $recipient = "drew_verwiel@outlook.com";
-  $subject = "Contact Form";
-
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-  echo "Thank You!";
+  
+  $mailTo = "drew_verwiel@outlook.com";
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+  
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: index.php?mailsend");
+  }
 ?>

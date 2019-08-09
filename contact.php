d34b008c2ded660c5d7d9d33cf4376e76a03c9d6
Drew <verwiel.code@gmail.com>
2019-08-09 11:03:15 -0600

    vaidation, simplistic php

---
+++ contact.php
@@ -0,0 +1,17 @@
+Thanks for your message!
+
+<?php
+	
+	$userName 		= $_POST['name'];
+  $userEmail	 	= $_POST['email'];
+  $userPhone    = $_POST['phone'];
+	$userMessage 		= $_POST['message'];
+	$to 			= "drew_verwiel@outlook.com";
+	$subject 		= "Intern Test Contact Form Submission";
+	$body 			= "Information Submitted:";
+	$body .= "\r\n Name: " . $userName;
+  $body .= "\r\n Email: " . $userEmail;
+  $body .= "\r\n Phone Number: " . $userPhone;
+	$body .= "\r\n Message: " . $userMessage;
+	mail($to, $subject, $body);
+?>
